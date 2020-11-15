e1432c66c45e46e7c459c1192836d8a08156a4ea
lzb30 <536543269@qq.com>
2020-11-15 17:22:47 +0800

    ts 改造代码

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
+import { RequestHandler } from "express";
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express typescript' });
+} as RequestHandler);
 
 module.exports = router;
